9595aba41f91e45b97a70e7feb012c75699a161a
Tushar <tendrako@uncc.edu>
2023-09-17 17:34:48 -0400

    Added server, Axios, and ChartJS

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use('/',express.static('public'))
 
 const budget = {
     myBudget: [
@@ -27,6 +28,10 @@ const budget = {
 
 app.get('/budget', (req, res) => {
     res.json(budget);
+});
+
+app.get('/hello', (req, res) => {
+    res.send("hello world man!!");
 });
 
 app.listen(port, () => {
