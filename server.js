07efa7de3b9fdf1899f226bb8377f05d5642b3a8
Tushar <tendrako@uncc.edu>
2023-09-17 19:09:39 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -6,29 +6,27 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
-app.use('/',express.static('public'))
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
+const fs = require('fs');
 
+// Define a function to read the budget data from a JSON file
+function readBudgetFromFile() {
+    try {
+        const budgetData = fs.readFileSync('budget.json', 'utf8');
+        return JSON.parse(budgetData);
+    } catch (err) {
+        console.error('Error reading budget data from file:', err);
+        return { myBudget: [] };
+    }
+}
 
+// Endpoint to get budget data from the JSON file
 app.get('/budget', (req, res) => {
+    const budget = readBudgetFromFile();
     res.json(budget);
 });
+
+app.use('/',express.static('public'))
 
 app.get('/hello', (req, res) => {
     res.send("hello world man!!");
